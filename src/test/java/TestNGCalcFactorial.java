613c3948bae1c390f63436ac30b8a56ac8e3d11e
Nikita Ilyuschenko <ilyusch.n@gmail.com>
2024-09-17 21:58:04 +0300

    solution reworked

--- src/test/java/TestNGCalcFactorial.java
+++ src/test/java/TestNGCalcFactorial.java
@@ -1,12 +1,19 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
-
 public class TestNGCalcFactorial {
-    @Test
-    void myTest (){
-        assertEquals(CalcFactorial.factorial(3),
-                3);
+    @Test(description = "Проверка позитивного значения (5)")
+    public void positiveSimpleTest() {
+        Assert.assertEquals(120, CalcFactorial.factorial(5));
+    }
+
+    @Test(description = "Проверка нуля")
+    public void positiveZeroTest() {
+        Assert.assertEquals(1, CalcFactorial.factorial(0));
+    }
+
+    @Test(description = "Проверка отрицательного значения (-1)")
+    public void negativeTest() {
+        Assert.assertThrows(IllegalArgumentException.class, () -> CalcFactorial.factorial(-1));
     }
 }
